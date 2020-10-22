1cd511c56c653a29e89409d8620de1a27604ce44
Felipe <felipequaquio@gmail.com>
2020-10-21 22:01:17 -0300

    Created routes to create and see details of orphanage (OrphanageCreate and Orphanage component).

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
 import OrphanagesMap from './pages/OrphanagesMap'
+import Orphanage from './pages/Orphanage'
+import CreateOrphanage from './pages/CreateOrphanage'
 
 function Routes() {
   return (
@@ -9,6 +11,8 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Landing}></Route>
         <Route path="/app" component={OrphanagesMap}></Route>
+        <Route path="/orphanages/create" component={CreateOrphanage}></Route>
+        <Route path="/orphanages/:id" component={Orphanage}></Route>
       </Switch>
     </BrowserRouter>
   )
